7b5978e81a18dfab0aa0efcf6e0d475ca56f05b3
Rami Kalai <rami@compose.im>
2022-03-23 16:07:23 +0000

    bumped go ver

--- example/main.go
+++ example/main.go
@@ -19,6 +19,7 @@ type Message struct {
 }
 
 func main() {
+	ch := make(chan interface{})
 	// bridge
 	go func() {
 		for msg := range pClient.Pusher.Messages {
@@ -32,12 +33,21 @@ func main() {
 			fmt.Println(msg)
 		}
 	}()
+
+	go sp()
+
+	<-ch
+}
+
+func sp() {
 	pClient.Pusher.StartPusher(
 		"3d41671bd9378ccdd519",              //pusher env id (this is dev)
 		"http://127.0.0.1:8090/pusher/auth", //authentication endpoint in capi
 		"private-my-channel",                // channel name
 		"auth",                              // user auth token issued by Compose
 	)
+
+	fmt.Println("STARTED PUSHER")
 }
 
 // ---------- Go Server ----------
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/QuanteriumTech/pusher-client
 
-go 1.17
+go 1.18
